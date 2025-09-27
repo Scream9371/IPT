e8338fd9f458a45755e58fe58beba16ad3d4528a
Yuxuan Mao <3135972956@qq.com>
2025-09-27 20:12:13 +0800

    Rename variables

--- run.m
+++ run.m
@@ -14,26 +14,26 @@ tran_cost = 0.001;
 load('Data Set\djia.mat');
 [n_periods, m_assets] = size(data);
 
-w_full_year = zeros(n_periods, m_assets);
-w_value_full_year = yar_weights(data, weight_inspect_wins);
-w_full_year(weight_inspect_wins + 1:n_periods, :) = w_value_full_year(:, :);
+yar_weights_full_wins = zeros(n_periods, m_assets);
+yar_weights_value_full_wins = yar_weights(data, weight_inspect_wins);
+yar_weights_full_wins(weight_inspect_wins + 1:n_periods, :) = yar_weights_value_full_wins(:, :);
 
-w_half_year = zeros(n_periods, m_assets);
-w_value_half_year = yar_weights(data, weight_inspect_wins / 2);
-w_half_year(weight_inspect_wins / 2 + 1:n_periods, :) = w_value_half_year(:, :);
+yar_weights_half_wins = zeros(n_periods, m_assets);
+yar_weights_value_half_wins = yar_weights(data, weight_inspect_wins / 2);
+yar_weights_half_wins(weight_inspect_wins / 2 + 1:n_periods, :) = yar_weights_value_half_wins(:, :);
 
 ratio = ubah_price_ratio(data);
 reverse_factor = 5;
 risk_factor = 5;
 
-active_factor_full_year = zeros(n_periods, 1);
-active_factor_value_full_year = yar_ubah(ratio(weight_inspect_wins - risk_inspect_wins + 1:n_periods, :), risk_inspect_wins);
-active_factor_full_year(weight_inspect_wins + 1:n_periods, 1) = active_factor_value_full_year(:, 1);
+yar_ubah_full_wins = zeros(n_periods, 1);
+yar_ubah_value_full_wins = yar_ubah(ratio(weight_inspect_wins - risk_inspect_wins + 1:n_periods, :), risk_inspect_wins);
+yar_ubah_full_wins(weight_inspect_wins + 1:n_periods, 1) = yar_ubah_value_full_wins(:, 1);
 
-active_factor_half_year = zeros(n_periods, 1);
-active_factor_value_half_year = yar_ubah(ratio(weight_inspect_wins / 2 - risk_inspect_wins / 2 + 1:n_periods, :), risk_inspect_wins / 2);
-active_factor_half_year(weight_inspect_wins / 2 + 1:n_periods, 1) = active_factor_value_half_year(:, 1);
+yar_ubah_half_wins = zeros(n_periods, 1);
+yar_ubah_value_half_wins = yar_ubah(ratio(weight_inspect_wins / 2 - risk_inspect_wins / 2 + 1:n_periods, :), risk_inspect_wins / 2);
+yar_ubah_half_wins(weight_inspect_wins / 2 + 1:n_periods, 1) = yar_ubah_value_half_wins(:, 1);
 
-[w_YAR, Q_factor] = active_function(w_value_full_year, w_value_half_year, active_factor_value_full_year, active_factor_value_half_year, data, weight_inspect_wins, reverse_factor, risk_factor);
+[w_YAR, Q_factor] = active_function(yar_weights_value_full_wins, yar_weights_value_half_wins, yar_ubah_value_full_wins, yar_ubah_value_half_wins, data, weight_inspect_wins, reverse_factor, risk_factor);
 
 [cum_wealth, daily_incre_fact, b_history] = IPT_run(data, win_size, tran_cost, w_YAR, Q_factor);
